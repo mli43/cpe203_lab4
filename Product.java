7962d34e39bf16dd2b3802ed39e904dfcaace0d8
mli43 <kate.li.0924@gmail.com>
2019-10-27 20:39:08 -0700

    added total price & set price methods

--- Product.java
+++ Product.java
@@ -1,9 +1,28 @@
 class Product{
     private final String productId;
     private double price;
+    private double avgPrice;
+    private double totalPrice;
 
     public Product(String productId, double price){
         this.price = price;
         this.productId = productId;
+        this.totalPrice = 0;
     }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public double getTotalPrice() {return totalPrice;}
+
+    public void setPrice(double newPrice){
+        totalPrice += newPrice;
+        price = newPrice;
+    }
+
 }
