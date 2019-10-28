0a37d84e4ba153cbba3f156925fd8afefd10c12d
mli43 <kate.li.0924@gmail.com>
2019-10-27 21:21:21 -0700

    refined constructor and added getters

--- Buy.java
+++ Buy.java
@@ -3,20 +3,14 @@ import java.util.List;
 import java.util.LinkedList;
 
 public class Buy{
-    private String sessionId;
     private String productId;
     private double price;
     private int quantity;
 
-    public Buy(String sessionId, String productId, int price, int quantity){
-        this.sessionId = sessionId;
+    public Buy(String productId, int price, int quantity){
         this.productId = productId;
         this.price = price;
         this.quantity = quantity;
-    }
-
-    public String getSessionId() {
-        return sessionId;
     }
 
     public String getProductId() {
